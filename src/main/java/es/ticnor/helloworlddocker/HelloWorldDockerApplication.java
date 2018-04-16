16d7fbdf517773ba5437fa0278d86edcefeb101f
Alejandro Villar <avillar@ticnor.es>
2018-04-16 13:34:46 +0200

    Configurar ServletInitializer

--- src/main/java/es/ticnor/helloworlddocker/HelloWorldDockerApplication.java
+++ src/main/java/es/ticnor/helloworlddocker/HelloWorldDockerApplication.java
@@ -2,9 +2,16 @@ package es.ticnor.helloworlddocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldDockerApplication {
+public class HelloWorldDockerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldDockerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldDockerApplication.class, args);
